021dba5fe9fc832c352129626bbf4b0446e0094f
sagar.saren <sagar.saren@cgi.com>
2019-09-04 12:02:42 +0530

    initial Commit

---
+++ Isogram.Tests/IsogramTests.cs
@@ -0,0 +1,67 @@
+﻿using Xunit;
+using System;
+using isogram_csharp_problem;
+
+namespace Isogram.Tests
+{
+    public class IsogramTests
+    {
+        [Fact]
+        public void TestWordIsogram()
+        {
+            bool expected = true;
+            bool actual = IsogramClass.Isogram("isogram");
+            Assert.Equal(expected,actual);
+        }
+        [Fact]
+        public void TestWordeleven()
+        {
+            bool expected = false;
+            bool actual = IsogramClass.Isogram("eleven");
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TestWordSubdermatoglyphicm()
+        {
+            bool expected = true;
+            bool actual = IsogramClass.Isogram("subdermatoglyphic");
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TestWordAlphabet()
+        {
+            bool expected = true;
+            bool actual = IsogramClass.Isogram("Alphabet");
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TestWordThumbscrew_japingly()
+        {
+            bool expected = true;
+            bool actual = IsogramClass.Isogram("thumbscrew-japingly");
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TestWordsix_year_old()
+        {
+            bool expected = true;
+            bool actual = IsogramClass.Isogram("six-year-old");
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TestWordsWithSpaces()
+        {
+            bool expected = true;
+            bool actual = IsogramClass.Isogram("Dipu great");
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestEmptyString()
+        {
+            bool expected = true;
+            bool actual = IsogramClass.Isogram("");
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ isogram-csharp-problem/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace isogram_csharp_problem
+{
+    public static class IsogramClass
+    {
+        public static bool Isogram(string phrase)
+        {
+            //var phraseInLowerCase = phrase.ToLower();
+            var phraseCharArray = phrase.ToCharArray();
+            var flag = true;
+            for(var i = 0; i<phraseCharArray.Length-1;i++)
+            {
+                if(phraseCharArray[i]>='a' && phraseCharArray[i]<='z')
+                {
+                    for(var j=i+1;j<phraseCharArray.Length;j++)
+                    {
+                        if(phraseCharArray[i]==phraseCharArray[j])
+                        {
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if(flag == false)
+                        break;
+                }
+            }
+            return flag;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phrase = Console.ReadLine();
+            //IsogramClass isogramClass = new IsogramClass();
+            Console.WriteLine(IsogramClass.Isogram(phrase));
+        }
+    }
+}
